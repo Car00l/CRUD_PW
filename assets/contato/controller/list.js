73412b4aefe5a36c1e20cb8ce2d79bf91c0c0ce7
4ngelo00 <gabrielangelo0320@gmail.com>
2023-03-30 10:40:03 -0300

    salvar

--- assets/contato/controller/list.js
+++ assets/contato/controller/list.js
@@ -4,9 +4,19 @@ $(document).ready(function(){
         dataType: "JSON",
         type: "POST",
         assync: true,
-        url: 'api/contato/model/list.php',
+        url: 'API/contato/model/list.php',
         success: function(dados){
-            
+            for(const resultado of dados){
+                $('tbody').append(`
+                    <tr>
+                        <td class="texte-center">${resultado.ID}</td>
+                        <td class="texte-center">${resultado.NOME}</td>
+                        <td class="texte-center">${resultado.TELEFONE}</td>
+                        <td class="texte-center">${resultado.CELULAR}</td>
+                        <td class="texte-center">${resultado.EMAIL}</td>
+                    </tr>
+                `)
+            }
         }
     })
 })
